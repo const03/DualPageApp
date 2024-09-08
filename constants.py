b1c4c51f3a74020c984172fc7464188a70393938
const03 <mariam.tsutskiridze10@gmail.com>
2024-09-08 23:05:06 +0400

    assignment 2

---
+++ constants.py
@@ -0,0 +1,57 @@
+RATES = {
+    ("USD", "EUR"): 0.9050,
+    ("USD", "GEL"): 2.6906,
+    ("USD", "GBP"): 0.7622,
+    ("USD", "USD"): 1,
+    ("EUR", "USD"): 1.1047,
+    ("EUR", "GEL"): 2.9699,
+    ("EUR", "GBP"): 0.8421,
+    ("EUR", "EUR"): 1,
+    ("GEL", "USD"): 0.3677,
+    ("GEL", "EUR"): 0.3327,
+    ("GEL", "GBP"): 0.2802,
+    ("GEL", "GEL"): 1,
+    ("GBP", "USD"): 1.3117,
+    ("GBP", "EUR"): 1.1871,
+    ("GBP", "GEL"): 3.5298,
+    ("GBP", "GBP"): 1,
+}
+
+CONVERTER_ERROR = "Error: Invalid amount"
+USERS = {
+    "admin": "admin"
+}
+DEFAULT_RESULT_VALUE = 0.0
+TAB = "    "
+RESULT_LABEL = "Result:" + TAB
+UI_FILE = "dual_page_app.ui"
+LOGIN_ERROR = "Invalid username or password. Please try again."
+LOGIN_BUTTON_RELEASED = ("border-style:outset;\n"
+                         "border-radius:10px;\n"
+                         "background-color:rgb(66,139,202);\n"
+                         "color:rgb(255,255,255)")
+
+LOGIN_BUTTON_PRESSED = ("border-style:outset;\n"
+                        "border-radius:10px;\n"
+                        "background-color:rgb(163, 199, 230);\n"
+                        "color:rgb(255,255,255)")
+
+LOGOUT_BUTTON_RELEASED = ("border-style:outset;\n"
+                          "border-radius:10px;\n"
+                          "background-color:rgb(153,153,153);\n"
+                          "color:rgb(255,255,255)")
+
+LOGOUT_BUTTON_PRESSED = ("border-style:outset;\n"
+                         "border-radius:10px;\n"
+                         "background-color: rgb(183, 183, 183);\n"
+                         "color:rgb(255,255,255)")
+
+CLEAR_OR_CONVERT_BUTTON_PRESSED = ("border-style:outset;\n"
+                                   "border-radius:10px;\n"
+                                   "background-color: rgb(100, 158, 101);\n"
+                                   "color:rgb(255,255,255)")
+
+CLEAR_OR_CONVERT_BUTTON_RELEASED = ("border-style:outset;\n"
+                                    "border-radius:10px;\n"
+                                    "background-color:rgb(60,118,61);\n"
+                                    "color:rgb(255,255,255)")
---
+++ dual_page_app.py
@@ -0,0 +1,103 @@
+from PyQt5 import QtWidgets
+from PyQt5 import uic
+import sys
+
+from constants import CONVERTER_ERROR, RATES, USERS, DEFAULT_RESULT_VALUE, RESULT_LABEL, UI_FILE, LOGIN_ERROR, \
+    LOGIN_BUTTON_RELEASED, LOGIN_BUTTON_PRESSED, LOGOUT_BUTTON_RELEASED, LOGOUT_BUTTON_PRESSED, \
+    CLEAR_OR_CONVERT_BUTTON_RELEASED, CLEAR_OR_CONVERT_BUTTON_PRESSED
+
+
+class UI(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+
+        # Load the ui file
+        uic.loadUi(UI_FILE, self)
+        self.stacked_widget.setCurrentWidget(self.login_page)
+
+        self.login_button.clicked.connect(self.login)
+        self.logout_button.clicked.connect(self.logout)
+        self.clear_button.clicked.connect(self.clear)
+        self.convert_button.clicked.connect(self.convert)
+
+        self.login_button.released.connect(self.login_button_released)
+        self.login_button.pressed.connect(self.login_button_pressed)
+
+        self.logout_button.released.connect(self.logout_button_released)
+        self.logout_button.pressed.connect(self.logout_button_pressed)
+
+        self.clear_button.released.connect(self.clear_button_released)
+        self.clear_button.pressed.connect(self.clear_button_pressed)
+
+        self.convert_button.released.connect(self.convert_button_released)
+        self.convert_button.pressed.connect(self.convert_button_pressed)
+
+        # show the app
+        self.show()
+
+    def login(self) -> None:
+        self.login_error_label.setText("")
+
+        username = self.username.text()
+        password = self.password.text()
+
+        if (username, password) in USERS.items():
+            self.stacked_widget.setCurrentWidget(self.converter_page)
+        else:
+            self.login_error_label.setText(LOGIN_ERROR)
+
+    def logout(self) -> None:
+        self.login_error_label.setText("")
+        self.username.setText("")
+        self.password.setText("")
+        self.stacked_widget.setCurrentWidget(self.login_page)
+        self.clear()
+
+    def clear(self) -> None:
+        self.amount.setText("")
+        self.result_label.setText(RESULT_LABEL + str(DEFAULT_RESULT_VALUE))
+        self.combo_1.setCurrentIndex(0)
+        self.combo_2.setCurrentIndex(0)
+        self.converter_error_label.setText("")
+
+    def convert(self) -> None:
+        self.converter_error_label.setText("")
+        from_currency = self.combo_1.currentText()
+        to_currency = self.combo_2.currentText()
+        try:
+            amount = float(self.amount.text())
+        except ValueError:
+            self.converter_error_label.setText(CONVERTER_ERROR)
+            return
+        rate = RATES[(from_currency, to_currency)]
+        ans = amount * rate
+        self.result_label.setText(RESULT_LABEL + str(ans)[:10])
+
+    def login_button_released(self) -> None:
+        self.login_button.setStyleSheet(LOGIN_BUTTON_RELEASED)
+
+    def login_button_pressed(self) -> None:
+        self.login_button.setStyleSheet(LOGIN_BUTTON_PRESSED)
+
+    def logout_button_released(self) -> None:
+        self.logout_button.setStyleSheet(LOGOUT_BUTTON_RELEASED)
+
+    def logout_button_pressed(self) -> None:
+        self.logout_button.setStyleSheet(LOGOUT_BUTTON_PRESSED)
+
+    def clear_button_released(self) -> None:
+        self.clear_button.setStyleSheet(CLEAR_OR_CONVERT_BUTTON_RELEASED)
+
+    def clear_button_pressed(self) -> None:
+        self.clear_button.setStyleSheet(CLEAR_OR_CONVERT_BUTTON_PRESSED)
+
+    def convert_button_released(self) -> None:
+        self.convert_button.setStyleSheet(CLEAR_OR_CONVERT_BUTTON_RELEASED)
+
+    def convert_button_pressed(self) -> None:
+        self.convert_button.setStyleSheet(CLEAR_OR_CONVERT_BUTTON_PRESSED)
+
+
+app = QtWidgets.QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
